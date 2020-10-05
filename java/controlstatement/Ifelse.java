513e9e3e1b24fb60046319b26f5ed07b904bfb91
pankajkaule <pankajkaule@gmail.com>
2020-10-05 19:47:05 +0530

    control-loops

---
+++ java/controlstatement/Ifelse.java
@@ -0,0 +1,14 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        // defining an 'age' variable
+        int age = 20;
+        // checking the age
+        if (age > 18) {
+            System.out.println("Age is greater than 18");
+        }else{
+            System.out.println("Age is less than 18");
+            
+        }
+    }
+
+}
