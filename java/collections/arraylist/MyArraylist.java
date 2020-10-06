eff4df2352f704c8a1e1bfbe5d277584b6bf9286
pankajkaule <pankajkaule@gmail.com>
2020-10-06 16:23:51 +0530

    readme added.

--- java/collections/arraylist/MyArraylist.java
+++ java/collections/arraylist/MyArraylist.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 public class MyArraylist {
     public static void main(String[] args) {
-        /*=========================================================================================================
-        |
-        |---------------------------------Linked List---------------------------------
-        |
-        |>>>>>>>LinkedList is datastructure of the datatype string
-        |==========================================================================================================*/
-        
-        LinkedList<String> al = new LinkedList<>();
-        al.add("Ravi");
-        al.add("Vijay");
-        al.add("Ravi");
-        al.add("Ajay");
-        Iterator<String> itr = al.iterator();
-        while (itr.hasNext()) {
-            System.out.println(itr.next());
-        }
+        /*
+         * =============================================================================
+         * ============================ | |---------------------------------Linked
+         * List--------------------------------- | |>>>>>>>LinkedList is datastructure
+         * of the datatype string
+         * |============================================================================
+         * ==============================
+         */
+
+        ArrayList<String> list = new ArrayList<>();// Creating arraylist
+        list.add("Mango");// Adding object in arraylist
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+        // Printing the arraylist object
+        System.out.println(list);
     }
 }
