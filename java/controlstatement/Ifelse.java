e1d3b698ab52303e9c9ba2b68ad1037635d37c56
pankajkaule <pankajkaule@gmail.com>
2020-10-06 10:19:34 +0530

    ifelse loop added.

--- java/controlstatement/Ifelse.java
+++ java/controlstatement/Ifelse.java
@@ -1,14 +1,9 @@
-public class Ifelse {
+class Ifelse {
     public static void main(String[] args) {
-        // defining an 'age' variable
-        int age = 20;
-        // checking the age
-        if (age > 18) {
-            System.out.println("Age is greater than 18");
-        }else{
-            System.out.println("Age is less than 18");
-            
-        }
+        int age=20;  
+    //checking the age  
+    if(age>18){  
+        System.out.print("Age is greater than 18\n");  
+    }  
     }
-
 }
